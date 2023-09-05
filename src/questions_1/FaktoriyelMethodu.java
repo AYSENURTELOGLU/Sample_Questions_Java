c5dffab26e867323a54ded54f569150afda081f4
AYSENURTELOGLU <aysenurteloglu@gmail.com>
2023-09-05 08:36:19 +0300

    20230905

---
+++ src/questions_1/FaktoriyelMethodu.java
@@ -0,0 +1,39 @@
+package questions_1;
+
+import java.util.Scanner;
+
+public class FaktoriyelMethodu {
+    public static void main(String[] args) {
+        /*
+        Soru 2:
+        Girdi olarak bir tamsayı kabul eden ve bu tam sayının faktöriyelini hesaplayan bir method yazın. Method bize
+        çıktıyı döndürsün. Örnek:
+        Girdi: 6
+        Çıktı: 6!=65432*1=720
+        ipucu: Aldıgınız sayıyı methoda gönderin. Methodun içinde For Loop ile faktöriyelini hesaplayın.
+         */
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lütfen bir tamsayı giriniz: ");
+        int sayi= scan.nextInt();
+
+        int faktoriyel=faktoriyel(sayi);
+
+        String metin="";
+        for (int i = 1; i < sayi; i++) {
+            metin+=i+"*";
+        }
+        System.out.println(sayi+"!"+"="+metin+"="+faktoriyel);
+    }
+    public static int faktoriyel(int sayi1){
+
+        int faktoriyel=1;
+        for (int i = 1; i <=sayi1 ; i++) {
+
+            faktoriyel*=i;
+
+        }
+        return faktoriyel;
+    }
+}
+
